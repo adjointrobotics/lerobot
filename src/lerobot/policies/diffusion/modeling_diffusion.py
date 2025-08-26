bfc39d41e5271312aa2e4ecd7c0a1519e2e6b9d7
Bryson Jones <bkjones97@gmail.com>
2025-08-26 13:57:56 -0700

    update diffusion policy to implement the same predict action chunk logic as act

--- src/lerobot/policies/diffusion/modeling_diffusion.py
+++ src/lerobot/policies/diffusion/modeling_diffusion.py
@@ -102,6 +102,15 @@ class DiffusionPolicy(PreTrainedPolicy):
     @torch.no_grad()
     def predict_action_chunk(self, batch: dict[str, Tensor]) -> Tensor:
         """Predict a chunk of actions given environment observations."""
+        self.eval()  # keeping the policy in eval mode as it could be set to train mode while queue is consumed
+
+        batch = self.normalize_inputs(batch)
+        if self.config.image_features:
+            batch = dict(batch)  # shallow copy so that adding a key doesn't modify the original
+            batch[OBS_IMAGES] = torch.stack([batch[key] for key in self.config.image_features], dim=-4)
+        # NOTE: It's important that this happens after stacking the images into a single key.
+        self._queues = populate_queues(self._queues, batch)
+
         # stack n latest observations from the queue
         batch = {k: torch.stack(list(self._queues[k]), dim=1) for k in batch if k in self._queues}
         actions = self.diffusion.generate_actions(batch)
@@ -136,13 +145,6 @@ class DiffusionPolicy(PreTrainedPolicy):
         # NOTE: for offline evaluation, we have action in the batch, so we need to pop it out
         if ACTION in batch:
             batch.pop(ACTION)
-
-        batch = self.normalize_inputs(batch)
-        if self.config.image_features:
-            batch = dict(batch)  # shallow copy so that adding a key doesn't modify the original
-            batch[OBS_IMAGES] = torch.stack([batch[key] for key in self.config.image_features], dim=-4)
-        # NOTE: It's important that this happens after stacking the images into a single key.
-        self._queues = populate_queues(self._queues, batch)
 
         if len(self._queues[ACTION]) == 0:
             actions = self.predict_action_chunk(batch)
